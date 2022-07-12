05c3515d5e9d65fbf5ce101be8dc0e6330448049
tenwishes <hateglock@gmail.com>
2022-07-12 19:25:35 +0300

    Updates

---
+++ precache-manifest.638a960e5843f853fc7eecd86802e458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0ff87b8e60ce605887f2321d28d59e",
+    "url": "/ignat-tasks/index.html"
+  },
+  {
+    "revision": "f86f4bbba2109cd00607",
+    "url": "/ignat-tasks/static/css/main.e7c9337b.chunk.css"
+  },
+  {
+    "revision": "d663bceb324f572e278f",
+    "url": "/ignat-tasks/static/js/2.23e4cfe2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ignat-tasks/static/js/2.23e4cfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86f4bbba2109cd00607",
+    "url": "/ignat-tasks/static/js/main.f1652521.chunk.js"
+  },
+  {
+    "revision": "248c99e198a055125b74",
+    "url": "/ignat-tasks/static/js/runtime-main.1e72cd74.js"
+  },
+  {
+    "revision": "f257469469193924404fd18a1bfefbf7",
+    "url": "/ignat-tasks/static/media/patterns.f2574694.svg"
+  }
+]);
